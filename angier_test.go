bb2ca24c87fc70b1e937057e7660688a07dcd4ba
Sam Helman <sam.helman@10gen.com>
2014-04-07 23:03:21 -0400

    Added test for different structs

--- angier_test.go
+++ angier_test.go
@@ -32,4 +32,56 @@ func TestTransfer(t *testing.T) {
 		})
 
 	})
+
+	Convey("With two structs of different types", t, func() {
+
+		type s1 struct {
+			// two matching fields
+			MatchOne int
+			MatchTwo bool
+
+			// two fields that don't appear in the other struct
+			OnlySrcOne int
+			OnlySrcTwo string
+
+			// two fields with the same name but the wrong type
+			WrongTypeOne int
+			WrongTypeTwo bool
+		}
+
+		type s2 struct {
+			// two matching fields
+			MatchOne int
+			MatchTwo bool
+
+			// two fields that don't appear in the other struct
+			OnlyDestOne int
+			OnlyDestTwo string
+
+			// two fields with the same name but the wrong type
+			WrongTypeOne bool
+			WrongTypeTwo int
+		}
+
+		Convey("transferring from one to the other should copy only the"+
+			" fields with the same name and type, and leave the other fields"+
+			" untouched", func() {
+
+			src := &s1{
+				MatchOne:     2,
+				MatchTwo:     true,
+				OnlySrcOne:   1,
+				OnlySrcTwo:   "hi",
+				WrongTypeOne: 3,
+				WrongTypeTwo: true,
+			}
+
+			dest := &s2{}
+
+			So(Transfer(src, dest), ShouldBeNil)
+			So(dest, ShouldResemble, &s2{MatchOne: 2, MatchTwo: true})
+
+		})
+
+	})
 }
